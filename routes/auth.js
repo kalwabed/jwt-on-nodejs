c1d9ca3e1a357aecf0dc7b39747b626294f7c741
kaliwa <kaalwabedrizki13@gmail.com>
2020-06-03 22:57:27 +0700

    login register, belum jwt

--- routes/auth.js
+++ routes/auth.js
@@ -1,20 +1,51 @@
 const router = require('express').Router()
 const User = require('../models/User')
-const { registerValidation } = require('../validation')
+const bcrypt = require('bcrypt')
+const { registerValidation, loginValidation } = require('../validation')
 
 router.post('/register', async (req, res) => {
+    const { username, email, password } = await req.body
+
+    // email checker
+    const emailExist = await User.findOne({ email })
+    if (emailExist) return res.status(400).send('Email is already exist.')
+
     // Data validator
     const { error } = registerValidation(req.body)
     if (error) return res.status(400).send(error.details[0].message)
 
-    // const { username, email, password } = await req.body
-    // const user = new User({ username, email, password })
-    // try {
-    //     const newUser = await user.save()
-    //     res.status(201).send(newUser)
-    // } catch (err) {
-    //     res.status(400).send(err)
-    // }
+    // Hash password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    try {
+        const user = new User({ username, email, password: hashedPassword })
+        await user.save()
+        res.status(201).send({
+            msg: 'Succesfully added a new user!',
+            id: user._id,
+        })
+    } catch (err) {
+        res.status(400).send(err)
+    }
+})
+
+router.post('/login', async (req, res) => {
+    const { email, password } = await req.body
+
+    // validation
+    const { error } = loginValidation(req.body)
+    if (error) return res.status(400).send(error.details[0].message)
+
+    // check email
+    const user = await User.findOne({ email })
+    if (!user) return res.status(400).send('Email not found!')
+
+    // check password
+    const validPass = await bcrypt.compare(password, user.password)
+    if (!validPass) return res.status(400).send('Password is incorrect!')
+
+    res.send('You has been signed in!')
 })
 
 module.exports = router
